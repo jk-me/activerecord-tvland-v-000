cb0c34c966e880ae0de63deea5d0dce637f32449
Jenny K <jkam17@gmail.com>
2018-09-22 16:58:36 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
+  def build_network(hash)
+    self.show.network.call_letters = hash[:call_letters]
+  end
 end
