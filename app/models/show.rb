dea48b7c6a18a206144b1ba59c772bd0e1d791bc
Jenny K <jkam17@gmail.com>
2018-09-22 16:58:55 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,6 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def build_network(hash)
-    self.show.network.call_letters = hash[:call_letters]
+    self.network.call_letters = hash[:call_letters]
   end
 end
